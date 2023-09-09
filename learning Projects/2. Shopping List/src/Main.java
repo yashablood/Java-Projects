9cde875badd2f2fac1a964b988b2007669a7846e
yashablood <yashablood@yahoo.com>
2023-09-09 10:13:44 -0600

    Add files via upload

---
+++ learning Projects/2. Shopping List/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("SHOPPING LIST\n" +
+              "\t- Milk\n" +
+              "\t\t> Semi-skimmed\n" +
+              "\t\t> Medium\n" +
+              "\t- Eggs\n" +
+              "\t\t> Free-range\n" +
+              "\t- Bread");
+    }
+
+}
